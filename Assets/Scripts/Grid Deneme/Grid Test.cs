c973b66f0ad1760c26a2fd8e3b062f282bdf7ea5
DinçerÖğren <emre.ogren@ogr.sakarya.edu.tr>
2023-12-08 00:04:47 +0300

    Grid system changes

--- Assets/Scripts/Grid Deneme/Grid Test.cs
+++ Assets/Scripts/Grid Deneme/Grid Test.cs
@@ -1,3 +1,4 @@
+using CodeMonkey.Utils;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,35 +6,58 @@ using UnityEngine;
 
 public class GridTest : MonoBehaviour
 {
+    public GameObject obje;
     Grid grid;
-    public int xLen, yLen, cellLen, cellHeight;
+    public int xLen, yLen, cellSize;
     // Start is called before the first frame update
     void Start()
     {
         
-        
+        grid = new Grid(xLen, yLen, cellSize,transform);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        grid = new Grid(xLen, yLen, cellLen, cellHeight);
-        for (int i = 0; i < yLen; i++)
+        for (int i = 0; i < xLen; i++)
         {
-            for (int j = 0; j < xLen; j++)
+            for (int j = 0; j < yLen; j++)
             {
-                DrawCube(grid.startPos + (grid.GetCellLength() * j * Vector3.right) + (grid.GetCellHeight() * i * Vector3.up), new Vector3(grid.GetCellLength(), grid.GetCellHeight()));
+               // grid.DrawCube(grid.gridStartPosition + (cellSize * i * Vector3.right) - (cellSize * j * Vector3.up), cellSize, grid.GetGridValue(i, j));
+                //UtilsClass.CreateWorldText(grid.GetGridValue(i, j).ToString(), null, grid.GetWorldPosition(i, j), 10, Color.white, TextAnchor.MiddleCenter);
+                Debug.LogWarning("Grid [" + i + "," + j + "] = " + grid.GetGridValue(i, j));
             }
         }
-        // DrawCube(Vector3.zero, new Vector3(grid.GetCellLength(), grid.GetCellHeight()));    
+        //DrawCube(Vector3.zero, new Vector3(grid.GetCellLength(), grid.GetCellHeight()));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 15);
+            //grid.GetGridValue(grid.GetWorldPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y));
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            grid.SetValue(0,0,2);
+            //grid.GetGridValue(grid.GetWorldPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y));
+        }
+        obje.transform.position = UtilsClass.GetMouseWorldPosition();
     }
 
-    private void DrawCube(Vector3 startPos,Vector3 size)
+    
+
+    private void DrawCbe(Vector3 startPos,float size)
     {
-        Debug.DrawLine(startPos,startPos + Vector3.right * size.x);
-        Debug.DrawLine(startPos + (size.x *Vector3.right), startPos + (size.x * Vector3.right) + Vector3.down * size.y);
-        Debug.DrawLine(startPos+ new Vector3(size.x,-size.y,0f), startPos + new Vector3(size.x, -size.y, 0f)+Vector3.left* size.x);
-        Debug.DrawLine(startPos + (size.y * Vector3.down), startPos);
+        //Debug.DrawLine(startPos,startPos + Vector3.right * size.x);
+        //Debug.DrawLine(startPos + (size.x *Vector3.right), startPos + (size.x * Vector3.right) + Vector3.down * size.y);
+        //Debug.DrawLine(startPos+ new Vector3(size.x,-size.y,0f), startPos + new Vector3(size.x, -size.y, 0f)+Vector3.left* size.x);
+        //Debug.DrawLine(startPos + (size.y * Vector3.down), startPos);
+
+        Debug.DrawLine(startPos, startPos + size * Vector3.right);
+        Debug.DrawLine(startPos, startPos + size * Vector3.down);
+        
     }
 
     private void OnDrawGizmos()
--- Assets/Scripts/Grid Deneme/Grid.cs
+++ Assets/Scripts/Grid Deneme/Grid.cs
@@ -1,40 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
+using CodeMonkey.Utils;
 
-public class Grid 
+public class Grid
 {
-    public Vector3 startPos = Vector3.zero;
+    public Vector3 gridStartPosition = Vector3.zero;
+
     int xLength;
     int yLength;
     int[,] gridArray;
-    int cellLength;
-    int cellHeight;
-    public Grid(int xLen, int yLen,int cellLength,int cellHeight)
+    int cellSize;
+    public Grid(int xLen, int yLen, int cellSize,Transform transform)
     {
+        gridStartPosition=  CalculateStartPosForThisObject(transform);
         xLength = xLen;
         yLength = yLen;
-        this.cellHeight = cellHeight;
-        this.cellLength = cellLength;
+        this.cellSize = cellSize;
 
         gridArray = new int[xLen, yLen];
 
-        for (int i = 0; i < gridArray.GetLength(1);i++)
+        for (int x = 0; x < gridArray.GetLength(0); x++)
         {
-            for (int j= 0; j < gridArray.GetLength(0); j++)
+            for (int y = 0; y < gridArray.GetLength(1); y++)
             {
-                Debug.Log(i + " " + j);
+                DrawCube(gridStartPosition + (cellSize * x * Vector3.right) + (cellSize * y * Vector3.up), cellSize, GetGridValue(x, y));
             }
         }
+        SetValue(1, 1, 1);
+    }
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+    public Vector3 CalculateStartPosForThisObject(Transform transform)
+    {
+        return  transform.position + new Vector3(-transform.localScale.x / 2, -transform.localScale.y / 2, 0);
     }
 
-    public float GetCellLength()
+    public void DrawCube(Vector3 startPos, float size,int value)
     {
-        return cellLength;
+        Color color = Color.cyan;
+        if (value == 0)
+        {
+            color = Color.white;
+        }
+        else if(value == 1)
+        {
+            color = Color.red;
+        }
+        Debug.DrawLine(startPos, startPos + Vector3.right * size,color,100f);
+        Debug.DrawLine(startPos + (size * Vector3.right), startPos + (size * Vector3.right) + Vector3.up * size, color, 100f);
+        Debug.DrawLine(startPos + new Vector3(size, size, 0f), startPos + new Vector3(size, size, 0f) + Vector3.left * size, color, 100f);
+        Debug.DrawLine(startPos, startPos+(size*Vector3.up), color, 100f);
+
+        //Debug.DrawLine(startPos, startPos + size * Vector3.right);
+        //Debug.DrawLine(startPos, startPos + size * Vector3.down);
+
     }
-    public float GetCellHeight()
+
+    public void SetValue(int x,int y,int value)
     {
-        return cellHeight;
+        if (x >= 0 && y >= 0 && x < xLength && y < yLength)
+        {
+            gridArray[x, y] = value;
+        }
     }
+    
+    public void SetValue(Vector3 worldPos,int value)
+    {
+        GetXY(worldPos, out int x, out int y);
+        SetValue(x, y, value);
+    }
+
+    private void GetXY(Vector3 worldPos,out int x,out int y)
+    {
+        x=Mathf.FloorToInt((worldPos.x - gridStartPosition.x )/ cellSize);
+        y = Mathf.FloorToInt((worldPos.y - gridStartPosition.y) / cellSize);
+    }
+    public Vector3 GetWorldPosition(int x,int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+    public int GetGridValue(int x,int y)
+    {
+        return gridArray[x, y];
+    }
+
 }
