ea74ebd54f854d13865d7dadfd225c7e506ef58e
DinçerÖğren <emre.ogren@ogr.sakarya.edu.tr>
2024-01-08 23:41:18 +0300

    Added PickUp script and new prefabs

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField] PickupType pickUpType;
+    [SerializeField] int pickupAmount = 1;
+
+    private void Update()
+    {
+        if (pickUpType == PickupType.Bomb)
+        {
+            GameManager.instance.AddBomb(pickupAmount);
+        }
+        else if (pickUpType == PickupType.Gold)
+        {
+            GameManager.instance.AddGold(pickupAmount);
+
+        }
+        else if (pickUpType == PickupType.PowerUp)
+        {
+            //we can add later
+        }
+    }
+
+
+}
+
+public enum PickupType
+{
+    Bomb,
+    Gold,
+    PowerUp
+}
